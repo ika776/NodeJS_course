5a16127f846fe0d018efe1bcfaaf84145a70319f
ika776 <ytian3280@gmail.com>
2022-03-05 23:00:35 +0700

    Handling HTTP GET Request

---
+++ express-demo/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+//Handling HTTP GET Request
+const courses =[
+    {id:1, name:'course1'},
+    {id:2, name:'course2'},
+    {id:3, name:'course3'}
+];
+
+//get all data
+app.get('/api/courses', (req,res)=> {
+    res.send(courses);
+})
+
+//get data by filter id
+app.get('/api/courses/:id', (req,res)=> {
+    const course = courses.find( c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send('The course is not available');
+    else res.send(course);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
