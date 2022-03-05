64d9cedffc6fa6edfb7e233edbc37fc2b42c591a
ika776 <ytian3280@gmail.com>
2022-03-05 10:16:39 +0700

    Node JS :Module System : Http Module

--- app.js
+++ app.js
@@ -60,15 +60,29 @@ clearInterval();*/
 // emitter.emit('Message logged')
 
 
-//Extending EventEmitter
-const EventEmitter = require('events');
-const Logger =require('./logger')
-const logger = new Logger();
+// //Extending EventEmitter
+// const EventEmitter = require('events');
+// const Logger =require('./logger')
+// const logger = new Logger();
 
-//Register a Listener
-logger.on('Message logged', (arg)=>{
-    console.log('Listener called', arg);
+// //Register a Listener
+// logger.on('Message logged', (arg)=>{
+//     console.log('Listener called', arg);
+// })
+
+// //Raise Event
+// logger.log('Message logged')
+
+const http = require('http');
+const server = http.createServer((req,res)=>{
+    if (req.url === '/'){
+        res.write('Hello world');
+        res.end();
+    }
+    if(req.url === '/api'){
+        res.write(JSON.stringify([1,2,3]))
+        res.end();
+    }
 })
-
-//Raise Event
-logger.log('Message logged')
+server.listen(3000)
+console.log('listening on port 3000');
