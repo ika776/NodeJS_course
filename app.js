ab7d7bfc7f3410f4ed421f45f801d4c3b42d7d23
ika776 <ytian3280@gmail.com>
2022-03-05 09:34:08 +0700

    Node JS :Module System : File System  Module

--- app.js
+++ app.js
@@ -42,3 +42,7 @@ const fs = require('fs');
 const files =fs.readdirSync('./')
 console.log(files);
 
+fs.readdir('./', function (err,file){
+    if(err) console.log('Error ',err);
+    else console.log('Results', file);
+})
